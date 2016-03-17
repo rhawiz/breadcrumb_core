62e18c08ebbcd015512d15695bf34ca5288def34
rhawiz <rawandhawiz@gmail.com>
2016-03-17 00:47:38 +0000

    webcollector complete

--- searchengines/google_search.py
+++ searchengines/google_search.py
@@ -1,24 +1,36 @@
+import json
+import random
 from time import sleep
-from urllib2 import Request
-
 import requests
 from bs4 import BeautifulSoup
+import socket
 
-from utils import is_ascii
-
-GOOGLE_WEB_SEARCH_URL = "https://www.google.com/search?q={}&start={}&num={}"
-
-REQUEST_HEADER = "Mozilla/5.0 (Windows NT x.y; rv:10.0) Gecko/20100101 Firefox/10.0"
+from netutils import generate_request_header, generate_proxy_dict
+from utils import is_ascii, get_hash
+import mechanize
 
 """
-    Google Image Search URL usage
-    Example URL: https://www.google.co.uk/search?q=SEARCH_QUERY&safe=off&nfpr=1&tbs=cdr:1,cd_min:01/03/2016,cd_max:10/03/2016,itp:face&tbm=isch
-    ----Relevant Parameters----
+    Google Web Search URL usage
+    Example: https://www.google.co.uk/search?q=SEARCH+QUERY&start=0&num=50&tbs=cdr:1,cd_min:01/03/2013,cd_max:16/03/2016
+    ---Parameters---
     q: Search Query
     tbs: Filters
         cdr:1 : Add time range filter
-            cd_min: Min Date
-            cd_max: Max Date
+            cd_min: Min Date (dd/mm/yyyy)
+            cd_max: Max Date (dd/mm/yyyy)
+
+"""
+GOOGLE_WEB_SEARCH_URL_BASE = "https://www.google.com/search?q={search_query}&start={start}&num={num}"
+
+"""
+    Google Image Search URL usage
+    Example URL: https://www.google.co.uk/search?q=SEARCH+QUERY&safe=off&nfpr=1&tbs=cdr:1,cd_min:01/03/2016,cd_max:10/03/2016,itp:face&tbm=isch
+    ----Parameters----
+    q: Search Query
+    tbs: Filters
+        cdr:1 : Add time range filter
+            cd_min: Min Date (dd/mm/yyyy)
+            cd_max: Max Date (dd/mm/yyyy)
         itp: Search Type
             face: Face Photos
             photo: Photograps
@@ -27,35 +39,73 @@ REQUEST_HEADER = "Mozilla/5.0 (Windows NT x.y; rv:10.0) Gecko/20100101 Firefox/1
             animated: GIFS
 
 """
-GOOGLE_IMAGE_SEARCH_URL = "https://www.google.co.uk/search?tbm=isch&q=SEARCH_QUERY&safe=off&nfpr=1&tbs=cdr:1,cd_min:{MIN_DATE},cd_max:10/03/2016,itp:{}"
+GOOGLE_IMAGE_SEARCH_URL_BASE = "https://www.google.co.uk/search?tbm=isch&q={search_query}&safe=off&nfpr=1"
+GOOGLE_IMAGE_SEARCH_FILTER_TYPE = "itp:{img_search_type}"
+
+GOOGLE_SEARCH_FILTER = "&tbs="
+GOOGLE_SEARCH_FILTER_DATE = "cdr:1,cd_min:{min_date},cd_max:{max_date}"
+
+socket.setdefaulttimeout(10)
 
 
 class GoogleWebSearch:
-    def __init__(self, query, wait=1, start=0, num=100, sentiment_analyser=None, debug=False):
+    def __init__(self, query, wait=-1, start=0, num=50, min_date=None, max_date=None, pages=1):
         self.query = query
         self.wait = wait
         self.start = start
         self.num = num
-        self.sentiment_analyser = sentiment_analyser
-        self.debug = debug
+        self.min_date = min_date
+        self.max_date = max_date
+        self.pages = pages
 
-    def search(self, pages=1):
-        results = []
-        for i in range(0, pages):
-            sleep(self.wait)
-            query = self.query
-            start = self.start + (i * self.num)
-            num = self.num
-            url = GOOGLE_WEB_SEARCH_URL.format(query, start, num)
-            request = Request(url)
+    def search(self):
+        self.results = []
+        self.global_hashes = []
+        self.stop_search = False
 
-            request.add_header('User-Agent', REQUEST_HEADER)
-            html_response = requests.get(url).text
+        page = 0
+        attempts = 0
+        while not self.stop_search and page < self.pages:
+            url = self._construct_url(page)
+            page += 1
+            header = generate_request_header()
+            br = mechanize.Browser()
+            proxy_dict = generate_proxy_dict()
+            try:
+                # request_response = br.open(url)
+                attempts += 1
+                request_response = requests.get(url, headers=header)
+
+            except Exception, e:
+                print e
+                continue
+
+            html_response = request_response.text
+            # html_response = request_response.read()
+
             content = self._scrape_page(html_response)
             if not len(content):
                 break
-            results += content
-        return results
+            self.results += content
+
+            if self.wait == -1:
+                wait_time = random.uniform(0.5, 2.5)
+                sleep(wait_time)
+
+        return self.results
+
+    def _construct_url(self, page):
+        query = self.query
+        start = self.start + (page * self.num)
+        num = self.num
+        BASE_URL = GOOGLE_WEB_SEARCH_URL_BASE.format(search_query=query, start=start, num=num)
+        min_date = self.min_date or ""
+        max_date = self.max_date or ""
+        if self.min_date or self.max_date:
+            BASE_URL += GOOGLE_SEARCH_FILTER + GOOGLE_SEARCH_FILTER_DATE
+            BASE_URL = BASE_URL.format(min_date=min_date, max_date=max_date)
+
+        return BASE_URL
 
     def _scrape_page(self, html):
         soup = BeautifulSoup(html, "html.parser")
@@ -63,35 +113,32 @@ class GoogleWebSearch:
         results = []
         for content in raw_content:
             link_tag = content.find('a')
-            link_url = link_tag.get('href')[7:]
-            print link_url.split('sa')
+            link_url = link_tag.get('href').split('&sa')
+            if not len(link_url):
+                continue
+            link_url = link_url[0]
+
             link_text = link_tag.get_text()
             short_text = content.find('span', attrs={'class': 'st'})
-
             if short_text is None:
                 continue
 
             short_text = short_text.get_text().encode('ascii', errors='ignore')
 
-            if self.debug:
-                try:
-                    print link_text, link_url, short_text[0:10]
-                except Exception:
-                    pass
-            content_analysis = None
+            # We'll hash the short text + link url, and if it exists in the list of global hashes,
+            #   then we will stop scraping as we've reached google's last page
+            result_hash = get_hash(short_text + link_url)
+            if result_hash in self.global_hashes:
+                self.stop_search = True
+                return []
+
+            self.global_hashes.append(result_hash)
 
             content = {
                 'url': link_url,
                 'url_text': link_text,
                 'text': short_text
             }
-
-            if self.sentiment_analyser:
-                try:
-                    content_analysis = self.sentiment_analyser(short_text)
-                    content['analysis'] = content_analysis
-                except Exception:
-                    pass
 
             results.append(content)
 
--- usercontentcollectors/webcollector.py
+++ usercontentcollectors/webcollector.py
@@ -1,23 +1,76 @@
 import json
 import os
+import re
 
+import requests
+import unicodedata
+from bs4 import BeautifulSoup, Comment
+
+from netutils import generate_request_header
 from searchengines.google_search import GoogleWebSearch
+
+from ai.sentimentanalyser import analyse_text as sa
 
 
 class WebCollector:
-    def __init__(self, aliases, sentiment_analyer=None):
+    def __init__(self, aliases, sentiment_analyer=None, min_date=None, max_date=None):
         self.aliases = aliases
         self.sentiment_analyser = sentiment_analyer
+        self.min_date = min_date
+        self.max_date = max_date
 
     def run(self):
-        content = []
+        self.content = []
         for alias in self.aliases:
-            google_search = GoogleWebSearch(query=alias, num=50, start=0)
-            content += google_search.search(2)
+            google_search = GoogleWebSearch(query=alias, num=25, start=0, pages=1,
+                                            min_date=self.min_date, max_date=self.max_date)
+            google_search_results = google_search.search()
+            for g_result in google_search_results:
+                url = g_result.get("url")
+                url_text = g_result.get("url_text")
+                short_text = g_result.get("text")
+                print url, url_text, short_text
+                try:
+                    response = requests.get(url, headers=generate_request_header())
+                except Exception, e:
+                    print e
+                    continue
+                html = response.text
+                if self.sentiment_analyser:
+                    if response.status_code == 200:
+                        analysis = self._analyse_html(html, alias)
+                        if not analysis:
+                            analysis = self.sentiment_analyser(short_text)
+                    else:
+                        analysis = self.sentiment_analyser(short_text)
 
-        return json.dumps(content)
+                output = g_result
+                output['alias'] = alias
+                output['analysis'] = analysis
+                self.content.append(output)
+        return self.content
+
+    def json(self):
+        return json.dump(self.content)
+
+    def _analyse_html(self, html, alias):
+        soup = BeautifulSoup(html, "html.parser")
+        texts = soup.findAll(text=True)
+        visible_text_list = filter(self._visible_element, texts)
+        visible_text = ''.join(visible_text_list)
+        visible_text = visible_text.replace('\n', ' ')
+        visible_text = re.sub(' +', ' ', visible_text)
+        visible_text = unicodedata.normalize('NFKD', visible_text).encode('ascii', 'ignore')
+        return self.sentiment_analyser(visible_text)
+
+    def _visible_element(self, element):
+        if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
+            return False
+        elif isinstance(element, Comment):
+            return False
+        return True
 
 
 if __name__ == "__main__":
-    wc = WebCollector(aliases=['Rawand Hawiz'])
+    wc = WebCollector(aliases=['rawand+hawiz'], sentiment_analyer=sa)
     print wc.run()
