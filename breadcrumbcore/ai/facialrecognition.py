2c19057c8aefe982fd165c39413107d87abf81a7
Rawand <rawandhawiz@gmail.com>
2016-03-27 19:39:36 +0100

    Google Image search and facial recognition

---
+++ breadcrumbcore/ai/facialrecognition.py
@@ -0,0 +1,54 @@
+import cv2
+import os
+import numpy
+
+# detect faces from an img and than take the face out, resize the face img to 200*200 gray format.
+def detect(filename, outfile=None):
+    face_cascade = cv2.CascadeClassifier('./cascades/haarcascade_frontalface_default.xml')
+    img = cv2.imread(filename)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        f = cv2.resize(gray[y:y+h, x:x+w], (200, 200))
+        if outfile:
+            outfile = "{}.{}".format(outfile,"jpg")
+            cv2.imwrite(outfile, f)
+        return f
+
+
+def read_images(path): #read training imgs in to array
+
+    ID = 0
+    imgList,faceID = [],[]
+    for filename in os.listdir(path):
+        filepath = os.path.join(path,filename)
+        img = cv2.imread(filepath,cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img,(200,200)) #resize imgs into 200*200 pixels
+        imgList.append(numpy.asarray(img, dtype=numpy.uint8))
+        faceID.append(ID)
+    print imgList
+    print faceID
+    return [imgList,faceID]
+
+def face_rec(training_img_path, img_path):
+
+    names = ['Iron Man', 'Dont Know']
+    [x,y] = read_images(training_img_path)
+    y = numpy.asarray(y, dtype=numpy.int32)
+    model = cv2.face.createEigenFaceRecognizer() #create face recognizer
+    model.train(numpy.asarray(x), numpy.asarray(y)) # train recognizer
+    face_cascade = cv2.CascadeClassifier('./cascades/haarcascade_frontalface_default.xml')
+
+    img = cv2.imread(img_path) # read img needs to be classified
+    faces= face_cascade.detectMultiscale(img, 1.3, 5)
+    for(x,y,w,h) in faces:
+        img = cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        roi = gray[x:x+w, y:y+h]
+        roi = cv2.resize(roi, (200,200), interpolation = cv2.INTER_LINEAR)
+        params = model.predict(roi)
+        return params
+
+if __name__ == "__main__":
+    img_path = 'RDJr.jpg'
--- breadcrumbcore/searchengines/googlesearch.py
+++ breadcrumbcore/searchengines/googlesearch.py
@@ -1,5 +1,7 @@
+import json
 import random
 import socket
+import urlparse
 from time import sleep
 
 import requests
@@ -20,26 +22,6 @@ from breadcrumbcore.utils.utils import get_hash8
 
 """
 GOOGLE_WEB_SEARCH_URL_BASE = "https://www.google.com/search?q={search_query}&start={start}&num={num}"
-
-"""
-    Google Image Search URL usage
-    Example URL: https://www.google.co.uk/search?q=SEARCH+QUERY&safe=off&nfpr=1&tbs=cdr:1,cd_min:01/03/2016,cd_max:10/03/2016,itp:face&tbm=isch
-    ----Parameters----
-    q: Search Query
-    tbs: Filters
-        cdr:1 : Add time range filter
-            cd_min: Min Date (dd/mm/yyyy)
-            cd_max: Max Date (dd/mm/yyyy)
-        itp: Search Type
-            face: Face Photos
-            photo: Photograps
-            clipart: Drawings
-            linedrawing: Line drawings
-            animated: GIFS
-
-"""
-GOOGLE_IMAGE_SEARCH_URL_BASE = "https://www.google.co.uk/search?tbm=isch&q={search_query}&safe=off&nfpr=1"
-GOOGLE_IMAGE_SEARCH_FILTER_TYPE = "itp:{img_search_type}"
 
 GOOGLE_SEARCH_FILTER = "&tbs="
 GOOGLE_SEARCH_FILTER_DATE = "cdr:1,cd_min:{min_date},cd_max:{max_date}"
@@ -63,11 +45,11 @@ class GoogleWebSearch:
         self.stop_search = False
 
         page = 0
-        attempts = 0
         while not self.stop_search and page < self.pages:
             url = self._construct_url(page)
             page += 1
             header = generate_request_header()
+            attempts = 0
             # proxy_dict = generate_proxy_dict()
             try:
                 # request_response = br.open(url)
@@ -148,5 +130,109 @@ class GoogleWebSearch:
         return results
 
 
+"""
+    Google Image Search URL usage
+    Example URL: https://www.google.co.uk/search?q=SEARCH+QUERY&safe=off&nfpr=1&tbs=cdr:1,cd_min:01/03/2016,cd_max:10/03/2016,itp:face&tbm=isch
+    ----Parameters----
+    q: Search Query
+    tbs: Filters
+        cdr:1 : Add time range filter
+            cd_min: Min Date (dd/mm/yyyy)
+            cd_max: Max Date (dd/mm/yyyy)
+        itp: Search Type
+            face: Face Photos
+            photo: Photograps
+            clipart: Drawings
+            linedrawing: Line drawings
+            animated: GIFS
+
+"""
+GOOGLE_IMAGE_SEARCH_BASE_URL = "https://www.google.co.uk/search?tbm=isch&q={search_query}&safe=off&nfpr=1&tbs=itp:{search_type}"
+
+
+
+
 class GoogleImageSearch:
-    pass
+    def __init__(self, query, wait=-1, start=0, min_date=None, max_date=None, num=50, search_type=None):
+        self.query = query
+        self.wait = wait
+        self.start = start
+        self.num = num
+        self.min_date = min_date
+        self.max_date = max_date
+        if search_type in ("all", "face", "photo", "clipart", "linedrawing", "animated"):
+            self.search_type = search_type
+        else:
+            self.search_type = "all"
+
+    def search(self):
+        results = []
+        stop_search = False
+        page = 0
+
+        while not stop_search:
+            url = self._construct_url()
+            print url
+            page += 1
+            header = generate_request_header()
+            # proxy_dict = generate_proxy_dict()
+            attempts = 0
+            try:
+                # request_response = br.open(url)
+                attempts += 1
+                request_response = requests.get(url, headers=header)
+
+            except Exception, e:
+                print e
+                continue
+
+            html_response = request_response.text
+            # html_response = request_response.read()
+
+            content = self._scrape_page(html_response)
+            results += content
+            # if self.wait == -1:
+            #     # Wait between 1.0 and 5.0 seconds before making the next request to prevent getting blocked.
+            #     wait_time = random.uniform(1.0, 5.0)
+            #     sleep(wait_time)
+
+            stop_search = True
+        return results
+
+    def _construct_url(self):
+        query = self.query
+        BASE_URL = GOOGLE_IMAGE_SEARCH_BASE_URL.format(search_query=query, search_type=self.search_type)
+        min_date = self.min_date or ""
+        max_date = self.max_date or ""
+        if self.min_date or self.max_date:
+            BASE_URL = "{}{}{}".format(BASE_URL,",",GOOGLE_SEARCH_FILTER_DATE)
+            BASE_URL = BASE_URL.format(min_date=min_date, max_date=max_date)
+
+        return BASE_URL
+
+    def _scrape_page(self, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        raw_content = soup.find_all('div', attrs={'class': 'rg_di rg_el ivg-i'})
+        results = []
+
+        for img_container in raw_content:
+            meta = img_container.find("div", attrs={"class":"rg_meta"})
+            meta_dict = json.loads(meta.contents[0])
+            url_text = meta_dict.get("pt")
+            short_text = meta_dict.get("s")
+            img_url = meta_dict.get("ou")
+            page_url = meta_dict.get("pt")
+            content = {
+                'img_url': img_url,
+                'page_url': page_url,
+                'url_text': url_text,
+                'text': short_text
+            }
+            results.append(content)
+
+        return results
+
+if __name__ == "__main__":
+    imgsrc = GoogleImageSearch(query="Rawand Hawiz",search_type="face")
+    content = imgsrc.search()
+    print content
