3dac0c05b8c039407fa33966b458e3f5f5e07d6c
Rawand <rawandhawiz@gmail.com>
2016-04-18 13:38:11 +0100

    facial rec

---
+++ breadcrumbcore/ai/face_rec.py
@@ -0,0 +1,120 @@
+import sys, os
+
+sys.path.append("../..")
+# import facerec modules
+from facerec.feature import Fisherfaces
+from facerec.distance import EuclideanDistance
+from facerec.classifier import NearestNeighbor
+from facerec.model import PredictableModel
+from facerec.validation import KFoldCrossValidation
+from facerec.visual import subplot
+from facerec.util import minmax_normalize
+import cv2
+# import numpy, matplotlib and logging
+import numpy as np
+from PIL import Image
+import matplotlib.cm as cm
+import logging
+
+def detect_face(filename, outfile=None):
+    face_cascade = cv2.CascadeClassifier('./cascades/haarcascade_frontalface_default.xml')
+    img = cv2.imread(filename)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        #img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        f = cv2.resize(gray[y:y + h, x:x + w], (200,200))
+
+        if outfile:
+            cv2.imwrite(outfile, f)
+        return f
+
+def read_images(path, sz=None):
+    """Reads the images in a given folder, resizes images on the fly if size is given.
+
+    Args:
+        path: Path to a folder with subfolders representing the subjects (persons).
+        sz: A tuple with the size Resizes
+
+    Returns:
+        A list [X,y]
+
+            X: The images, which is a Python list of numpy arrays.
+            y: The corresponding labels (the unique number of the subject, person) in a Python list.
+    """
+    c = 0
+    X, y = [], []
+    key_map = []
+    for dirname, dirnames, filenames in os.walk(path):
+        for subdirname in dirnames:
+            subject_path = os.path.join(dirname, subdirname)
+            subject_count = 0
+            for filename in os.listdir(subject_path):
+                try:
+                    temp_path = os.path.join(subject_path, "temp_%s" % filename)
+                    path = os.path.join(subject_path, filename)
+                    temp_file = detect_face(path, temp_path)
+                    im = Image.open(temp_path)
+                    im = im.convert("L")
+                    os.remove(temp_path)
+                    # resize to given size (if given)
+                    if (sz is not None):
+                        im = im.resize(sz, Image.ANTIALIAS)
+                    X.append(np.asarray(im, dtype=np.uint8))
+                    y.append(c)
+                    subject_count+=1
+                except IOError, e:
+                    pass
+                except:
+                    print "Unexpected error:", sys.exc_info()[0]
+                    raise
+            print "%s->%s (%s)" % (subdirname, c,subject_count)
+            key_map.append(subdirname)
+
+            c = c + 1
+    return [X, y, key_map]
+
+
+if __name__ == "__main__":
+    # This is where we write the images, if an output_dir is given
+    # in command line:
+    out_dir = None
+    # You'll need at least a path to your image data, please see
+    # the tutorial coming with this source code on how to prepare
+    # your image data:
+
+    # Now read in the image data. This must be a valid path!
+    [X, y, key_map] = read_images("./faces")
+    # Then set up a handler for logging:
+    feature = Fisherfaces()
+    # Define a 1-NN classifier with Euclidean Distance:
+    classifier = NearestNeighbor(dist_metric=EuclideanDistance(), k=1)
+    # Define the model as the combination
+    model = PredictableModel(feature=feature, classifier=classifier)
+    # Compute the Fisherfaces on the given data (in X) and labels (in y):
+    model.compute(X, y)
+
+    # Then turn the first (at most) 16 eigenvectors into grayscale
+    # images (note: eigenvectors are stored by column!)
+    # E = []
+    # for i in xrange(min(model.feature.eigenvectors.shape[1], 16)):
+    #     e = model.feature.eigenvectors[:, i].reshape(X[0].shape)
+    #     E.append(minmax_normalize(e, 0, 255, dtype=np.uint8))
+
+    img_path = 'shiran2.jpg'
+    coverted_img_path = "temp_%s" % img_path
+    detect_face(img_path,outfile=coverted_img_path)
+    img = Image.open(coverted_img_path)
+    img = img.convert("L")
+    cvImg = cv2.imread(img_path)
+
+
+    p = model.predict(img)[0]
+    label = key_map[p]
+    print label
+    print key_map
+
+    cv2.imshow(label,cvImg)
+    cv2.waitKey()
