e8f6eb2328d54ddc3bf1dde5ed823a6e3e59b806
rhawiz <rawandhawiz@gmail.com>
2016-04-18 18:46:05 +0100

    cleaned up facial recognition

--- breadcrumbcore/ai/face_rec.py
+++ breadcrumbcore/ai/face_rec.py
@@ -22,7 +22,6 @@ def detect_face(filename, outfile=None):
     face_cascade = cv2.CascadeClassifier('./cascades/haarcascade_frontalface_default.xml')
     img = cv2.imread(filename)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
         #img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
--- breadcrumbcore/ai/facialrecognition.py
+++ breadcrumbcore/ai/facialrecognition.py
@@ -24,6 +24,7 @@ from facerec.model import PredictableModel
 from facerec.validation import KFoldCrossValidation
 from facerec.serialization import save_model, load_model
 
+
 # This is the face recognition module for the RESTful Webservice.
 #
 # The current implementation uses a fixed model defined in code. 
@@ -35,14 +36,13 @@ from facerec.serialization import save_model, load_model
 # the training labels. It also supports updating a model, instead of 
 # re-training it. 
 class PredictableModelWrapper(object):
-
     def __init__(self, model):
         self.model = model
         self.numeric_dataset = NumericDataSet()
-        
+
     def compute(self):
-        X,y = self.numeric_dataset.get()
-        self.model.compute(X,y)
+        X, y = self.numeric_dataset.get()
+        self.model.compute(X, y)
 
     def set_data(self, numeric_dataset):
         self.numeric_dataset = numeric_dataset
@@ -73,7 +73,7 @@ class PredictableModelWrapper(object):
 # Now define a method to get a model trained on a NumericDataSet,
 # which should also store the model into a file if filename is given.
 def get_model(numeric_dataset, model_filename=None):
-    feature = ChainOperator(Resize((128,128)), Fisherfaces())
+    feature = ChainOperator(Resize((128, 128)), Fisherfaces())
     classifier = NearestNeighbor(dist_metric=EuclideanDistance(), k=1)
     inner_model = PredictableModel(feature=feature, classifier=classifier)
     model = PredictableModelWrapper(inner_model)
@@ -83,6 +83,7 @@ def get_model(numeric_dataset, model_filename=None):
         save_model(model_filename, model)
     return model
 
+
 # Now a method to read images from a folder. It's pretty simple,
 # since we can pass a numeric_dataset into the read_images  method 
 # and just add the files as we read them. 
@@ -91,7 +92,7 @@ def read_images(path, identifier, numeric_dataset):
         try:
             img_path = os.path.abspath(os.path.join(path, filename))
             temp_path = os.path.abspath(os.path.join(path, "temp_%s" % filename))
-            detect_face(img_path,temp_path)
+            detect_face(img_path, temp_path)
             img = Image.open(temp_path)
             img = img.convert("L")
             img = np.asarray(img, dtype=np.uint8)
@@ -103,6 +104,30 @@ def read_images(path, identifier, numeric_dataset):
         except:
             print "Unexpected error:", sys.exc_info()[0]
             raise
+
+
+def read_image(path, identifier, numeric_dataset):
+    try:
+        path_parts = path.split(".")
+
+        if path_parts[-1] not in ("jpg","jpeg"):
+            return
+
+        img_path = os.path.abspath(path)
+        temp_path = os.path.abspath("temp.%s" % path_parts[-1])
+        detect_face(img_path, temp_path)
+        img = Image.open(temp_path)
+        img = img.convert("L")
+        img = np.asarray(img, dtype=np.uint8)
+        numeric_dataset.add(identifier, img)
+        print "%s -> %s" % (img_path, identifier)
+        os.remove(temp_path)
+    except IOError, (errno, strerror):
+        print "I/O error({0}): {1}".format(errno, strerror)
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+        raise
+
 
 # read_csv is a tiny little method, that takes a csv file defined
 # like this:
@@ -121,16 +146,20 @@ def read_from_csv(filename):
             read_images(path, identifier, numeric_dataset)
     return numeric_dataset
 
-def get_numeric_dataset(path, identifier):
+
+def get_numeric_dataset(image_paths, identifier):
     numeric_dataset = NumericDataSet()
-    read_images(path, identifier, numeric_dataset)
+    for path in image_paths:
+        read_image(path, identifier, numeric_dataset)
     return numeric_dataset
+
 
 # Just some sugar on top...
 def get_model_from_csv(filename, out_model_filename=None):
     numeric_dataset = read_from_csv(filename)
     model = get_model(numeric_dataset, out_model_filename)
     return model
+
 
 def load_model_file(model_filename):
     load_model(model_filename)
@@ -140,29 +169,39 @@ def load_model_file(model_filename):
 # If multiple faces detect, it'll return the first one identified
 def detect_face(filename, outfile=None):
     face_cascade = cv2.CascadeClassifier('./cascades/haarcascade_frontalface_default.xml')
+
+    if not os.path.isfile(filename):
+        return
+
     img = cv2.imread(filename)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(img, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
     if len(faces):
-        face = faces[0]
+        x, y, w, h = faces[0]
+        cropped = gray[y:y + h, x:x + w]
         if outfile:
-            cv2.imwrite(outfile, face)
-        return face
+            cv2.imwrite(outfile, cropped)
+        return cropped
+
 
 if __name__ == "__main__":
-    model = get_model_from_csv("faces.csv")
-    img_path = 'rawand1.jpg'
+    img_path = 'kim1.jpg'
     coverted_img_path = "temp_%s" % img_path
-    detect_face(img_path,outfile=coverted_img_path)
+    detect_face(img_path, outfile=coverted_img_path)
+    model = get_model_from_csv("faces.csv")
     img = Image.open(coverted_img_path)
     img = img.convert("L")
     p = model.predict(img)
-    print p
+    print "*******", p
 
     new_numeric_dataset = read_from_csv("new_faces.csv")
     model.add_data(new_numeric_dataset)
 
+    print model.numeric_dataset.data.keys()
+
     p = model.predict(img)
-    print p
+    print "*******", p
+
