774bdc2f4c9af05820bf946c5052316215443797
rhawiz <rawandhawiz@gmail.com>
2016-04-18 17:21:12 +0100

    cleaned up facial recognition

--- breadcrumbcore/ai/face_rec.py
+++ breadcrumbcore/ai/face_rec.py
@@ -12,6 +12,8 @@ from facerec.util import minmax_normalize
 import cv2
 # import numpy, matplotlib and logging
 import numpy as np
+from facerec.dataset import NumericDataSet
+
 from PIL import Image
 import matplotlib.cm as cm
 import logging
@@ -22,16 +24,14 @@ def detect_face(filename, outfile=None):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
     for (x, y, w, h) in faces:
         #img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         f = cv2.resize(gray[y:y + h, x:x + w], (200,200))
-
         if outfile:
             cv2.imwrite(outfile, f)
         return f
 
-def read_images(path, sz=None):
+def read_images(path, sz=None, keys=[]):
     """Reads the images in a given folder, resizes images on the fly if size is given.
 
     Args:
@@ -44,16 +44,16 @@ def read_images(path, sz=None):
             X: The images, which is a Python list of numpy arrays.
             y: The corresponding labels (the unique number of the subject, person) in a Python list.
     """
-    c = 0
+    c = len(keys)
     X, y = [], []
-    key_map = []
+
     for dirname, dirnames, filenames in os.walk(path):
         for subdirname in dirnames:
             subject_path = os.path.join(dirname, subdirname)
             subject_count = 0
             for filename in os.listdir(subject_path):
                 try:
-                    temp_path = os.path.join(subject_path, "temp_%s" % filename)
+                    temp_path = os.path.abspath(os.path.join(subject_path, "temp_%s" % filename))
                     path = os.path.join(subject_path, filename)
                     temp_file = detect_face(path, temp_path)
                     im = Image.open(temp_path)
@@ -71,22 +71,53 @@ def read_images(path, sz=None):
                     print "Unexpected error:", sys.exc_info()[0]
                     raise
             print "%s->%s (%s)" % (subdirname, c,subject_count)
-            key_map.append(subdirname)
+            keys.append(subdirname)
 
             c = c + 1
-    return [X, y, key_map]
+    return [X, y, keys]
 
+class PredictableModelWrapper(object):
+
+    def __init__(self, model):
+        self.model = model
+        self.numeric_dataset = NumericDataSet()
+
+    def compute(self):
+        X,y = self.numeric_dataset.get()
+        self.model.compute(X,y)
+
+    def set_data(self, numeric_dataset):
+        self.numeric_dataset = numeric_dataset
+
+    def predict(self, image):
+        prediction_result = self.model.predict(image)
+        # Only take label right now:
+        num_label = prediction_result[0]
+        str_label = self.numeric_dataset.resolve_by_num(num_label)
+        return str_label
+
+    def update(self, name, image):
+        self.numeric_dataset.add(name, image)
+        class_label = self.numeric_dataset.resolve_by_str(name)
+        extracted_feature = self.feature.extract(image)
+        self.classifier.update(extracted_feature, class_label)
+
+    def __repr__(self):
+        return "PredictableModelWrapper (Inner Model=%s)" % (str(self.model))
 
 if __name__ == "__main__":
     # This is where we write the images, if an output_dir is given
     # in command line:
+
     out_dir = None
     # You'll need at least a path to your image data, please see
     # the tutorial coming with this source code on how to prepare
     # your image data:
 
     # Now read in the image data. This must be a valid path!
-    [X, y, key_map] = read_images("./faces")
+    [X, y, keys] = read_images("../faces/")
+    print len(X), keys
+
     # Then set up a handler for logging:
     feature = Fisherfaces()
     # Define a 1-NN classifier with Euclidean Distance:
@@ -103,18 +134,22 @@ if __name__ == "__main__":
     #     e = model.feature.eigenvectors[:, i].reshape(X[0].shape)
     #     E.append(minmax_normalize(e, 0, 255, dtype=np.uint8))
 
-    img_path = 'shiran2.jpg'
+
+    img_path = 'rawand1.jpg'
     coverted_img_path = "temp_%s" % img_path
     detect_face(img_path,outfile=coverted_img_path)
     img = Image.open(coverted_img_path)
     img = img.convert("L")
-    cvImg = cv2.imread(img_path)
+
+    p = model.predict(img)[0]
+    label = keys[p]
+    print label
+
+    [X, y, keys] = read_images("../faces2/", keys=keys)
+    model.classifier.update(X,y)
 
 
     p = model.predict(img)[0]
-    label = key_map[p]
+    label = keys[p]
     print label
-    print key_map
 
-    cv2.imshow(label,cvImg)
-    cv2.waitKey()
--- breadcrumbcore/ai/facialrecognition.py
+++ breadcrumbcore/ai/facialrecognition.py
@@ -1,63 +1,168 @@
-import cv2
-import os
-import numpy
-import facerec
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# detect faces from an img and than take the face out, resize the face img to 200*200 gray format.
-def detect(filename, outfile=None):
+# Copyright (c) Philipp Wagner. All rights reserved.
+# Licensed under the BSD license. See LICENSE file in the project root for full license information.
+
+import csv, os, sys
+# Import PIL
+import cv2
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+# Import numpy:
+import numpy as np
+# Import facerec:
+from facerec.feature import ChainOperator, Fisherfaces
+from facerec.preprocessing import Resize
+from facerec.dataset import NumericDataSet
+from facerec.distance import EuclideanDistance
+from facerec.classifier import NearestNeighbor
+from facerec.model import PredictableModel
+from facerec.validation import KFoldCrossValidation
+from facerec.serialization import save_model, load_model
+
+# This is the face recognition module for the RESTful Webservice.
+#
+# The current implementation uses a fixed model defined in code. 
+# A simple wrapper works around a limitation of the current framework, 
+# because as time of writing this only integer labels can be passed into 
+# the classifiers of the facerec framework. 
+
+# This wrapper hides the complexity of dealing with integer labels for
+# the training labels. It also supports updating a model, instead of 
+# re-training it. 
+class PredictableModelWrapper(object):
+
+    def __init__(self, model):
+        self.model = model
+        self.numeric_dataset = NumericDataSet()
+        
+    def compute(self):
+        X,y = self.numeric_dataset.get()
+        self.model.compute(X,y)
+
+    def set_data(self, numeric_dataset):
+        self.numeric_dataset = numeric_dataset
+
+    def predict(self, image):
+        prediction_result = self.model.predict(image)
+        # Only take label right now:
+        num_label = prediction_result[0]
+        str_label = self.numeric_dataset.resolve_by_num(num_label)
+        return str_label
+
+    def add_data(self, new_numeric_dataset):
+        data = new_numeric_dataset.data
+        for name, img_list in data.iteritems():
+            for img in img_list:
+                self.update(name, img)
+
+    def update(self, name, image):
+        self.numeric_dataset.add(name, image)
+        class_label = self.numeric_dataset.resolve_by_str(name)
+        extracted_feature = self.model.feature.extract(image)
+        self.model.classifier.update(extracted_feature, class_label)
+
+    def __repr__(self):
+        return "PredictableModelWrapper (Inner Model=%s)" % (str(self.model))
+
+
+# Now define a method to get a model trained on a NumericDataSet,
+# which should also store the model into a file if filename is given.
+def get_model(numeric_dataset, model_filename=None):
+    feature = ChainOperator(Resize((128,128)), Fisherfaces())
+    classifier = NearestNeighbor(dist_metric=EuclideanDistance(), k=1)
+    inner_model = PredictableModel(feature=feature, classifier=classifier)
+    model = PredictableModelWrapper(inner_model)
+    model.set_data(numeric_dataset)
+    model.compute()
+    if not model_filename is None:
+        save_model(model_filename, model)
+    return model
+
+# Now a method to read images from a folder. It's pretty simple,
+# since we can pass a numeric_dataset into the read_images  method 
+# and just add the files as we read them. 
+def read_images(path, identifier, numeric_dataset):
+    for filename in os.listdir(path):
+        try:
+            img_path = os.path.abspath(os.path.join(path, filename))
+            temp_path = os.path.abspath(os.path.join(path, "temp_%s" % filename))
+            detect_face(img_path,temp_path)
+            img = Image.open(temp_path)
+            img = img.convert("L")
+            img = np.asarray(img, dtype=np.uint8)
+            numeric_dataset.add(identifier, img)
+            print "%s -> %s" % (filename, identifier)
+            os.remove(temp_path)
+        except IOError, (errno, strerror):
+            print "I/O error({0}): {1}".format(errno, strerror)
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            raise
+
+# read_csv is a tiny little method, that takes a csv file defined
+# like this:
+#
+#   Philipp Wagner;D:/images/philipp
+#   Another Name;D:/images/another_name
+#   ...
+#
+def read_from_csv(filename):
+    numeric_dataset = NumericDataSet()
+    with open(filename, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='#')
+        for row in reader:
+            identifier = row[0]
+            path = row[1]
+            read_images(path, identifier, numeric_dataset)
+    return numeric_dataset
+
+def get_numeric_dataset(path, identifier):
+    numeric_dataset = NumericDataSet()
+    read_images(path, identifier, numeric_dataset)
+    return numeric_dataset
+
+# Just some sugar on top...
+def get_model_from_csv(filename, out_model_filename=None):
+    numeric_dataset = read_from_csv(filename)
+    model = get_model(numeric_dataset, out_model_filename)
+    return model
+
+def load_model_file(model_filename):
+    load_model(model_filename)
+
+
+# Detect and return cropped faces in an image. Returns empty array if none detect.
+# If multiple faces detect, it'll return the first one identified
+def detect_face(filename, outfile=None):
     face_cascade = cv2.CascadeClassifier('./cascades/haarcascade_frontalface_default.xml')
     img = cv2.imread(filename)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x, y, w, h) in faces:
-        img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        f = cv2.resize(gray[y:y + h, x:x + w], (200, 200))
-        if outfile:
-            outfile = "{}.{}".format(outfile, "jpg")
-            cv2.imwrite(outfile, f)
-        return f
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
-def read_images(path):  # read training imgs in to array
-
-    id = 1
-    img_list, label = [], []
-    for filename in os.listdir(path):
-        if "jm" in filename:
-            id=1
-        else:
-            id=0
-        filepath = os.path.join(path, filename)
-        img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
-        img = cv2.resize(img, (200, 200))  # resize imgs into 200*200 pixels
-        img_list.append(numpy.asarray(img, dtype=numpy.uint8))
-        label.append(id)
-    return [img_list, label]
-
-
-def face_rec(training_img_path, img_path):
-    [x, y] = read_images(training_img_path)
-    y = numpy.asarray(y, dtype=numpy.int32)
-
-    model = cv2.face.createEigenFaceRecognizer()  # create face recognizer
-    model.train(numpy.asarray(x), numpy.asarray(y))  # train recognizer
-    face_cascade = cv2.CascadeClassifier('./cascades/haarcascade_frontalface_default.xml')
-    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)  # read img needs to be classified
     faces = face_cascade.detectMultiScale(img, 1.3, 5)
-    for (x, y, w, h) in faces:
-        img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        try:
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        except Exception:
-            gray = img
-        roi = gray[x:x + w, y:y + h]
-        roi = cv2.resize(roi, (200, 200), interpolation=cv2.INTER_LINEAR)
-        params = model.predict(roi)
-        return params
-
+    if len(faces):
+        face = faces[0]
+        if outfile:
+            cv2.imwrite(outfile, face)
+        return face
 
 if __name__ == "__main__":
-    print cv2.__version__
-    img_path = 'rawand.jpg'
-    training_imgs = "./faces"
-    print face_rec(training_imgs, img_path)
+    model = get_model_from_csv("faces.csv")
+    img_path = 'rawand1.jpg'
+    coverted_img_path = "temp_%s" % img_path
+    detect_face(img_path,outfile=coverted_img_path)
+    img = Image.open(coverted_img_path)
+    img = img.convert("L")
+    p = model.predict(img)
+    print p
+
+    new_numeric_dataset = read_from_csv("new_faces.csv")
+    model.add_data(new_numeric_dataset)
+
+    p = model.predict(img)
+    print p
