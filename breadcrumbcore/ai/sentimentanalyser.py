cca781da054bc645432a22bf884667cb236efe06
Rawand <rawandhawiz@gmail.com>
2016-04-23 16:15:20 +0100

    updated version

--- breadcrumbcore/ai/sentimentanalyser.py
+++ breadcrumbcore/ai/sentimentanalyser.py
@@ -17,23 +17,22 @@ def analyse_text(text_list):
     counter = 0
     for text in text_list:
         print text
-        if is_ascii(text):
-            text = text.strip()
-            data = 'text={}'.format(text)
-            response = requests.post(url=url, data=data)
-            if response:
-                try:
-                    out = response.json()
-                    out_neg = float(out["probability"]["neg"])
-                    out_neutral = float(out["probability"]["neutral"])
-                    out_pos = float(out["probability"]["pos"])
+        text = text.strip()
+        data = 'text={}'.format(text)
+        response = requests.post(url=url, data=data)
+        if response:
+            try:
+                out = response.json()
+                out_neg = float(out["probability"]["neg"])
+                out_neutral = float(out["probability"]["neutral"])
+                out_pos = float(out["probability"]["pos"])
 
-                    neg += out_neg
-                    neutral += out_neutral
-                    pos += out_pos
-                    counter += 1
-                except Exception:
-                    pass
+                neg += out_neg
+                neutral += out_neutral
+                pos += out_pos
+                counter += 1
+            except Exception:
+                pass
 
     if counter > 0:
         neg /= counter
